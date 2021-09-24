c8faef1e6d999e23d7b5f0f0e6dee463f03f3ad9
ariscss <aris.candra.s@gmail.com>
2021-09-24 17:33:08 +0700

    Update Input Type Email

--- app/src/main/java/com/example/barbers/MainActivity3.java
+++ app/src/main/java/com/example/barbers/MainActivity3.java
@@ -1,13 +1,25 @@
 package com.example.barbers;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity3 extends AppCompatActivity {
+    Button Next;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main3);
+        Next = findViewById(R.id.button);
+        Next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity3.this, LoginActivity.class));
+            }
+        });
     }
 }
